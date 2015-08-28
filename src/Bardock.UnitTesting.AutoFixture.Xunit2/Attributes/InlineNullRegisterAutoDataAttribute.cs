afe52eeb8b9e20f19eaa99c442f44fccf2d11ba3
Pablo Morelli <pmorelli@originsoftware.com.ar>
2015-08-28 15:59:42 -0300

    - Added NullRegisterCustomization - Added InlineNullRegisterAutoDataAttribute

---
+++ src/Bardock.UnitTesting.AutoFixture.Xunit2/Attributes/InlineNullRegisterAutoDataAttribute.cs
@@ -0,0 +1,54 @@
+﻿using Bardock.UnitTesting.AutoFixture.Customizations;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Bardock.UnitTesting.AutoFixture.Xunit2.Attributes
+{
+    /// <summary>
+    /// Makes null the parameter that has a name that matches with the
+    /// parameterName provided.
+    /// </summary>
+    public abstract class InlineNullRegisterAutoDataAttribute : InlineAutoDataAttribute
+    {
+        private string _parameterName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InlineNullRegisterAutoDataAttribute"/> class.
+        /// </summary>
+        /// <param name="autoDataAttribute">The <see cref="AutoDataAttribute"/> instance that provides auto-generated data specimens.</param>
+        /// <param name="parameterName">Name of the null parameter.</param>
+        public InlineNullRegisterAutoDataAttribute(
+            AutoDataAttribute autoDataAttribute,
+            string parameterName)
+            : base(autoDataAttribute)
+        {
+            _parameterName = parameterName;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest)
+        {
+            var parameter = methodUnderTest.GetParameters().First(p => p.Name == _parameterName);
+            var fixture = AutoDataAttribute.Fixture;
+
+            var nullCustomization = ((ICustomization)
+                                            Activator
+                                                .CreateInstance(typeof(NullRegisterCustomization<>)
+                                                .MakeGenericType(parameter.ParameterType)));
+
+            fixture.Customize(nullCustomization);
+            fixture.Customize<NullRegisterArgument>(e => e.With(p => p.Name, _parameterName));
+
+            return base.GetData(methodUnderTest);
+        }
+
+    }
+
+    public class NullRegisterArgument
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Bardock.UnitTesting.AutoFixture/Customizations/NullRegisterCustomization.cs
@@ -0,0 +1,13 @@
+﻿using Ploeh.AutoFixture;
+
+namespace Bardock.UnitTesting.AutoFixture.Customizations
+{
+    public class NullRegisterCustomization<T> : ICustomization
+        where T : class
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Register<T>(() => null);
+        }
+    }
+}
