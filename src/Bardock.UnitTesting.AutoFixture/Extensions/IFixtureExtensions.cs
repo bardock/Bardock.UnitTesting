e6704b65b75344fc806f0ab9a3dc328d37f38bf5
rsevil <rodrigo.sevil@gmail.com>
2015-09-25 15:46:23 -0300

    Added RegisterLazy Extension Methods | Bardock.UnitTesting.AutoFixture 0.3.0

--- src/Bardock.UnitTesting.AutoFixture/Extensions/IFixtureExtensions.cs
+++ src/Bardock.UnitTesting.AutoFixture/Extensions/IFixtureExtensions.cs
@@ -3,6 +3,7 @@ using Ploeh.AutoFixture.Dsl;
 using Ploeh.AutoFixture.Kernel;
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace Bardock.UnitTesting.AutoFixture.Extensions
 {
@@ -41,6 +42,47 @@ namespace Bardock.UnitTesting.AutoFixture.Extensions
                 .Where(xf => xf is ICustomizationComposer<T>)
                 .Cast<ICustomizationComposer<T>>()
                 .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Registers a lazy creation function for a specific type.
+        /// The <paramref name="T"/> will be created when first requested.
+        /// The generated instance is thread safe.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fixture">The fixture.</param>
+        /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
+        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator)
+        {
+            fixture.RegisterLazy(creator, isThreadSafe: false);
+        }
+
+        /// <summary>
+        /// Registers a lazy creation function for a specific type.
+        /// The <paramref name="T"/> will be created when first requested.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fixture">The fixture.</param>
+        /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
+        /// <param name="isThreadSafe">true to make this instance usable concurrently by multiple threads; false to make this instance usable by only one thread at a time.</param>
+        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator, bool isThreadSafe)
+        {
+            var lazy = new Lazy<T>(creator, isThreadSafe);
+            fixture.Register<T>(() => lazy.Value);
+        }
+
+        /// <summary>
+        /// Registers a lazy creation function for a specific type.
+        /// The <paramref name="T"/> will be created when first requested.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fixture">The fixture.</param>
+        /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
+        /// <param name="mode">One of the enumeration values that specifies the <see cref="LazyThreadSafetyMode"/>.</param>
+        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator, LazyThreadSafetyMode mode)
+        {
+            var lazy = new Lazy<T>(creator, mode);
+            fixture.Register<T>(() => lazy.Value);
         }
     }
 }
