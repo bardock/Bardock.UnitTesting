b1420d3177e4c6e135b31e480ad0ea4706e0dc7f
rsevil <rodrigo.sevil@gmail.com>
2015-10-07 11:09:09 -0300

    Bardock.UnitTesting.AutoFixture 0.4.0 (Renamed RegisterLazy to RegisterFrozen)

--- src/Bardock.UnitTesting.AutoFixture/Extensions/IFixtureRegisterExtensions.cs
+++ src/Bardock.UnitTesting.AutoFixture/Extensions/IFixtureRegisterExtensions.cs
@@ -7,43 +7,8 @@ using System.Threading;
 
 namespace Bardock.UnitTesting.AutoFixture.Extensions
 {
-    public static class IFixtureExtensions
+    public static class IFixtureRegisterExtensions
     {
-        /// <summary>
-        /// Customizes the specified composer transformation.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="fixture">The fixture.</param>
-        /// <param name="composerTransformation">The composer transformation.</param>
-        /// <param name="append">if set to <c>true</c> [append].</param>
-        public static void Customize<T>(this IFixture fixture, Func<ICustomizationComposer<T>, ISpecimenBuilder> composerTransformation, bool append)
-        {
-            if (append)
-            {
-                var composer = fixture.GetCurrentCustomizationComposer<T>();
-                if (composer != null)
-                {
-                    fixture.Customize<T>(b => composerTransformation(composer));
-                    return;
-                }
-            }
-            fixture.Customize(composerTransformation);
-        }
-
-        /// <summary>
-        /// Gets the current customization composer.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="fixture">The fixture.</param>
-        /// <returns></returns>
-        public static ICustomizationComposer<T> GetCurrentCustomizationComposer<T>(this IFixture fixture)
-        {
-            return fixture.Customizations
-                .Where(xf => xf is ICustomizationComposer<T>)
-                .Cast<ICustomizationComposer<T>>()
-                .FirstOrDefault();
-        }
-
         /// <summary>
         /// Registers a lazy creation function for a specific type.
         /// The <paramref name="T"/> will be created when first requested.
@@ -52,9 +17,9 @@ namespace Bardock.UnitTesting.AutoFixture.Extensions
         /// <typeparam name="T"></typeparam>
         /// <param name="fixture">The fixture.</param>
         /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
-        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator)
+        public static void RegisterFrozen<T>(this IFixture fixture, Func<T> creator)
         {
-            fixture.RegisterLazy(creator, isThreadSafe: false);
+            fixture.RegisterFrozen(creator, isThreadSafe: false);
         }
 
         /// <summary>
@@ -65,7 +30,7 @@ namespace Bardock.UnitTesting.AutoFixture.Extensions
         /// <param name="fixture">The fixture.</param>
         /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
         /// <param name="isThreadSafe">true to make this instance usable concurrently by multiple threads; false to make this instance usable by only one thread at a time.</param>
-        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator, bool isThreadSafe)
+        public static void RegisterFrozen<T>(this IFixture fixture, Func<T> creator, bool isThreadSafe)
         {
             var lazy = new Lazy<T>(creator, isThreadSafe);
             fixture.Register<T>(() => lazy.Value);
@@ -79,7 +44,7 @@ namespace Bardock.UnitTesting.AutoFixture.Extensions
         /// <param name="fixture">The fixture.</param>
         /// <param name="creator">A function that will be used to create objects of type T every time the Ploeh.AutoFixture.Fixture is asked to create an object of that type.</param>
         /// <param name="mode">One of the enumeration values that specifies the <see cref="LazyThreadSafetyMode"/>.</param>
-        public static void RegisterLazy<T>(this IFixture fixture, Func<T> creator, LazyThreadSafetyMode mode)
+        public static void RegisterFrozen<T>(this IFixture fixture, Func<T> creator, LazyThreadSafetyMode mode)
         {
             var lazy = new Lazy<T>(creator, mode);
             fixture.Register<T>(() => lazy.Value);
---
+++ src/Bardock.UnitTesting.AutoFixture/Extensions/IListOfSpecimenBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Bardock.UnitTesting.AutoFixture.Extensions
+{
+    public static class IListOfSpecimenBuilderExtensions
+    {
+        public static void AddToTop(this IList<ISpecimenBuilder> list, ISpecimenBuilder builder)
+        {
+            list.Insert(0, builder);
+        }
+    }
+}
